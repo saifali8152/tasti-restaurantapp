120139c8d86df5184c7b6dde626b7fe21ce2bf2a
UmarKhaann <MuhammadUmar62792@gmail.com>
2025-09-08 10:20:12 +0500

    updated

--- lib/features/screens.dart
+++ lib/features/screens.dart
@@ -28,7 +28,6 @@ export 'restaurant-admin/food_menu/presentation/pages/food_menu.dart';
 export 'restaurant-admin/seating_area/presentation/pages/seating_area.dart';
 export 'restaurant-admin/reservations_db/presentation/pages/customer_reservations.dart';
 export 'restaurant-admin/gallery/presentation/pages/gallery.dart';
-export 'restaurant-admin/dashboard/dashboard_screen.dart';
 export 'common/skaleton/widgets/custom_drawer.dart';
 export 'restaurant-admin/reservations/presentation/pages/reservations.dart';
 export 'admin/campaigns/presentation/pages/campaign_by_res_details.dart';
